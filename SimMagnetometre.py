b773607aaf7da5addf5a3db964317f9f91a3448f
LouisLSTL <ardoisetarneclouis@gmail.com>
2025-03-25 13:31:42 +0000

    demoEtEncapsulation

--- SimMagnetometre.py
+++ SimMagnetometre.py
@@ -1,54 +1,76 @@
 import numpy as np
-import matplotlib.pyplot as plt
+
 # coords : (x,y) 
-p = 1
-r = np.array((3,0))
-pn,ps = np.array((.5,0)),np.array((-.5,0))
-alpha = 15
-retard = alpha*np.pi/180
-def rotMatr(theta,v):
-    return np.matmul(np.array([
-        (np.cos(theta),-np.sin(theta)),
-        (np.sin(theta),np.cos(theta))]
-    ),v)
-def H(r,ps,pn):
-    l = pn-ps
-    m = np.dot(p,l)
-    A =  3*r*(np.dot(m,r))
-    B = np.linalg.norm(r)**5
-    C = m/(np.linalg.norm(r)**3)
-    return ((A)/(B) - (C))/(4*np.pi)
+def base(r=np.array((3,0)),p=1,thetas = np.linspace(0,6*np.pi,1000)):
+    pn,ps = np.array((.5,0)),np.array((-.5,0))
+    def rotMatr(theta,v):
+        return np.matmul(np.array([
+            (np.cos(theta),-np.sin(theta)),
+            (np.sin(theta),np.cos(theta))]
+        ),v)
+    def H(r,ps,pn):
+        l = pn-ps
+        m = np.dot(p,l)
+        A =  3*r*(np.dot(m,r))
+        B = np.linalg.norm(r)**5
+        C = m/(np.linalg.norm(r)**3)
+        return ((A)/(B) - (C))/(4*np.pi)
+
+    pN = [rotMatr(thetas[i],pn) for i in range(len(thetas))]
+    pS = [rotMatr(thetas[i],ps) for i in range(len(thetas))]
+    h = [H(r,pS[i],pN[i]) for i in range(len(pS))] 
+
+    N = np.array([np.linalg.norm(i) for i in h])
+    a =  np.array([np.arctan2(*i) for i in h])
+    return N,a
+
+def sim(a,r=np.array((3,0)),p=1,thetas = np.linspace(0,6*np.pi,1000)):
+    pn,ps = np.array((.5,0)),np.array((-.5,0))
+    alpha = a
+    retard = alpha*np.pi/180
+    def rotMatr(theta,v):
+        return np.matmul(np.array([
+            (np.cos(theta),-np.sin(theta)),
+            (np.sin(theta),np.cos(theta))]
+        ),v)
+    def H(r,ps,pn):
+        l = pn-ps
+        m = np.dot(p,l)
+        A =  3*r*(np.dot(m,r))
+        B = np.linalg.norm(r)**5
+        C = m/(np.linalg.norm(r)**3)
+        return ((A)/(B) - (C))/(4*np.pi)
+
+    pN = [rotMatr(thetas[i],pn) for i in range(len(thetas))]
+    pS = [rotMatr(thetas[i],ps) for i in range(len(thetas))]
+    h = [H(r,pS[i],pN[i]) for i in range(len(pS))] 
+
+    pN2 = np.array([rotMatr(thetas[i]-retard,pn) for i in range(len(thetas))])
+    pS2 = np.array([rotMatr(thetas[i]-retard,ps) for i in range(len(thetas))])
+    h2 = np.array([H(r,pS2[i],pN2[i]) for i in range(len(pS2))])
+
+    N = np.array([np.linalg.norm(i) for i in h])
+    a =  np.array([np.arctan2(*i) for i in h])
+    N2 =  np.array([np.linalg.norm(i) for i in h2])
+    a2 =  np.array([np.arctan2(*i) for i in h2])
+    htot = h+h2
+    Ntot = np.array([np.linalg.norm(i) for i in htot])
+    atot = np.array([np.arctan2(*i) for i in htot])
+
+    return Ntot,atot
+    """fig=plt.figure()
 
 
-thetas = np.linspace(0,6*np.pi,1000)
-pN = [rotMatr(thetas[i],pn) for i in range(len(thetas))]
-pS = [rotMatr(thetas[i],ps) for i in range(len(thetas))]
-h = [H(r,pS[i],pN[i]) for i in range(len(pS))] 
+    ax1=fig.add_subplot(2,1,1)
+    ax1.plot(thetas,Ntot, 'r')
+    ax1.set_title('Norm of measured magnetic field')
+    ax1.set_xlabel(r'$\theta (rad)$')
+    ax2=fig.add_subplot(2,1,2)
+    ax2.plot(thetas,atot ,'b')
+    ax2.set_title('Angle measured magnetic field direction')
+    ax2.set_xlabel(r'$\theta (rad)$')
 
-pN2 = [rotMatr(thetas[i]-retard,pn) for i in range(len(thetas))]
-pS2 = [rotMatr(thetas[i]-retard,ps) for i in range(len(thetas))]
-h2 = [H(r,pS2[i],pN2[i]) for i in range(len(pS2))] 
-
-N = np.array([np.linalg.norm(i) for i in h])
-a =  np.array([np.arctan2(*i) for i in h])
-N2 =  np.array([np.linalg.norm(i) for i in h2])
-a2 =  np.array([np.arctan2(*i) for i in h2])
-htot = h+h2
-Ntot = np.array([np.linalg.norm(i) for i in htot])
-atot = np.array([np.arctan2(*i) for i in htot])
-fig=plt.figure()
-
-
-ax1=fig.add_subplot(2,1,1)
-ax1.plot(thetas,Ntot, 'r')
-ax1.set_title('Norm of measured magnetic field')
-ax1.set_xlabel(r'$\theta (rad)$')
-ax2=fig.add_subplot(2,1,2)
-ax2.plot(thetas,atot ,'b')
-ax2.set_title('Angle measured magnetic field direction')
-ax2.set_xlabel(r'$\theta (rad)$')
-
-fig.suptitle(r'pour $\theta$'+f' = {alpha} deg') 
-plt.tight_layout()
-plt.show()
-plt.savefig('sim.png')
+    fig.suptitle(r'pour $\theta$'+f' = {alpha} deg') 
+    plt.tight_layout()
+    plt.show()
+    plt.savefig('sim.png')"""
---
+++ demoAngles.py
@@ -0,0 +1,10 @@
+from SimMagnetometre import sim,base
+import numpy as np
+import matplotlib.pyplot as plt
+thetas = np.linspace(0,6*np.pi,1000)
+for n in range(0,180,25):
+    plt.plot(thetas,sim(n)[0],label=r'$\theta$'+f' = {n} deg')
+plt.plot(thetas,base()[0],'k--',label='just stirrer')
+plt.legend()
+plt.grid()
+plt.savefig('demo.png')
